3fe37efcbf346f87639221cbb1bcdc5bed353f2d
SHYFT <sean@poweredbyshyft.com>
2026-01-04 14:59:38 -0500

    Bump version to 1.0.23

--- acf-events-calendar.php
+++ acf-events-calendar.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ACF Events Calendar
  * Description: Custom events calendar with FullCalendar.io integration for ACF-powered events
- * Version: 1.0.22
+ * Version: 1.0.23
  * Author: SHYFT
  * Requires PHP: 7.4
  * GitHub Plugin URI: shyft-marketing/acf-events-calendar
